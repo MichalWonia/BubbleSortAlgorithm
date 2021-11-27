42d14b4a36ba634ae8c5a74e029c07af1d2d0580
Michal Wonia <spodek.mw@gmail.com>
2021-11-27 12:11:22 +0100

    Fixed some bugs. Bubble sort function works. Project finished.

--- src/BubbleSortAlgorithm.java
+++ src/BubbleSortAlgorithm.java
@@ -2,6 +2,20 @@ public class BubbleSortAlgorithm {
 
     public static void main(String[] args) {
 
+        int[] unsortedArray = {5, 13, 1, 6, 98, 0, 12, 23, 65, 11, 12};
+        int[] sortedArray;
+
+        System.out.println("Unsorted array:");
+        for (Integer integer : unsortedArray) {
+            System.out.print(integer + " ");
+        }
+
+        sortedArray = bubbleSort(unsortedArray);
+
+        System.out.println("\nSorted array: ");
+        for (Integer integer : sortedArray) {
+            System.out.print(integer + " ");
+        }
     }
 
     public static int[] bubbleSort(int[] inputArray) {
@@ -9,11 +23,11 @@ public class BubbleSortAlgorithm {
         int storage;
 
         for (int i = 0; i < inputArray.length; i++) {
-            for (int j = 0; j < inputArray.length - i; j++) {
-                if(inputArray[j+1] > inputArray[j]){
-                    storage = inputArray[j+1];
-                    inputArray[j] = inputArray[j+1];
-                    inputArray[j] = storage;
+            for (int j = 0; j < inputArray.length - i - 1; j++) {
+                if (inputArray[j + 1] > inputArray[j]) {
+                    storage = inputArray[j];
+                    inputArray[j] = inputArray[j + 1];
+                    inputArray[j + 1] = storage;
                 }
             }
         }
